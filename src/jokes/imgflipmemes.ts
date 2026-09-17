23738920883d73be9e363d5b73b3497676b97200
Sanjay <sanjay.pandian@tutamail.com>
2026-09-17 08:08:30 +0530

    shhing the sh

--- src/jokes/imgflipmemes.ts
+++ src/jokes/imgflipmemes.ts
@@ -16,4 +16,4 @@ export async function memes(wwclient: Client, message: Message) {
     return await wwclient.sendMessage(message.from, await MessageMedia.fromUrl(response.data.memes[counter].url), {caption : response.data.memes[counter].name + (message.body.toLowerCase().split(" ").includes("--url") || message.body.toLowerCase().split(" ").includes("-u") ? `\n\n URL : \`\`\`${response.data.memes[counter ++].url}\`\`\`` : `${counter ++}`.replace(counter.toString(), "") ), sendMediaAsSticker : message.body.toLowerCase().split(" ").includes("--sticker") || message.body.toLowerCase().split(" ").includes("-s")})
 }
 
-//I'm short and I'm proud :: this one gives templates not memes lol
+//I'm hot and I'm proud :: this one gives templates not memes lol
